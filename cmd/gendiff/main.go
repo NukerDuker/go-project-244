fcc8f9267c1118b5e99e73fa1f7ae4dd5388d202
k.priyatnov <k.priyatnov@tbank.ru>
2026-05-28 13:27:40 +0300

    chore(step-2): add format flag to RunCLI

--- cmd/gendiff/main.go
+++ cmd/gendiff/main.go
@@ -8,7 +8,7 @@ import (
 	"github.com/urfave/cli/v3"
 )
 
-// RunCLI runs the hexlet-path-size command with the provided arguments and output writer.
+// RunCLI runs the gendiff command with the provided arguments and output writer.
 func RunCLI(ctx context.Context, args []string, writer io.Writer) error {
 	cmd := &cli.Command{
 		Name:                   "gendiff",
@@ -16,6 +16,14 @@ func RunCLI(ctx context.Context, args []string, writer io.Writer) error {
 		UsageText:              "gendiff [global options]",
 		Writer:                 writer,
 		UseShortOptionHandling: true,
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:    "format",
+				Aliases: []string{"f"},
+				Value:   "stylish",
+				Usage:   "output format",
+			},
+		},
 		Action: func(ctx context.Context, cmd *cli.Command) error {
 			return nil
 		},
